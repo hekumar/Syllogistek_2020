ad7d35286adc603bf2390eb53595858c4e7e1e49
unknown <Hemant Kumar@parijat.fareast.corp.microsoft.com>
2020-10-07 16:36:47 +0530

    fix1

--- src/app/components/banner-container/banner-container.component.ts
+++ src/app/components/banner-container/banner-container.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { EnrollFormComponent } from '../enroll-form/enroll-form.component';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-banner-container',
   templateUrl: './banner-container.component.html',
-  styleUrls: ['./banner-container.component.scss']
+  styleUrls: ['./banner-container.component.scss'],
+  animations: [trigger('list1', [
+
+    transition('void => *', [
+      style({
+        opacity: 0,
+      }),
+      animate(300)
+    ]),
+    transition('* => void', [
+      animate(200, style({
+        opacity: 0
+      }))
+    ])
+  ])
+  ]
 })
 export class BannerContainerComponent implements OnInit {
 
