2c18c1350b2dd5963b2b0acabe7cc40a89ecec6e
unknown <Hemant Kumar@parijat.fareast.corp.microsoft.com>
2021-01-02 13:34:06 +0530

    lazy loading

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { LoadImgDirective } from './load-img.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoadImgDirective],
   imports: [
     CommonModule,
     NgbModule
